b0e266669c4a574553fdc4bbe9c98c1f1763b8d4
Suganya J <159772807+suganyajulianna@users.noreply.github.com>
2024-11-29 12:20:21 +0530

    Delete dist/optimuspage directory

--- dist/optimuspage/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{b as a}from"./chunk-FUX7J24E.mjs";import"./chunk-QPFJFPMW.mjs";import"./chunk-3G23GWB5.mjs";import"./chunk-GN3HINFO.mjs";import"./chunk-E44LSF4W.mjs";export{a as default};
